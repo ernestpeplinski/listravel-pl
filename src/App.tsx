b3784503f811610f8eb847b444dc408bd137f599
Ernest Peplinski <ernestpeplinski@gmail.com>
2025-07-12 20:04:16 +0200

    Use React + Vite to bulid webside

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import logo from './assets/logo_1000x1000.png'
+import './App.css'
+
+function App() {
+  return (
+    <div id="root">
+      <img src={logo} className="logo" alt="Logo" />
+      <h2>Strona w budowie</h2>
+      <p>Będzie dostępna wkrótce.</p>
+    </div>
+  )
+}
+
+export default App
