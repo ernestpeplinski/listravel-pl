5fbfd7628bf7ec2da6d5f8c9608c77ad9ef61aed
Ernest Peplinski <ernestpeplinski@gmail.com>
2025-11-04 15:50:35 +0100

    Add new trips

--- src/data/trips.tsx
+++ src/data/trips.tsx
@@ -2,10 +2,14 @@ import borneSulinowoImg from "../assets/trips/26.10.2025 Borne Sulinowo.jpg";
 import cieszynImg from "../assets/trips/8.11.2025 Cieszyn.jpg";
 import lichenImg from "../assets/trips/15.11.2025 Lichen.jpg";
 import pragaImg from "../assets/trips/13.12.2025 Praga.jpg";
+import pruszczImg from "../assets/trips/22.11.2025 Pruszcz Gdanski.jpg";
+import jarmarkTorunBgdImg from "../assets/trips/6.12.2025 Bydgoszcz Torun Jarmark.jpg";
 import borneSulinowoThumb from "../assets/trips/thumbnails/borneSulinowo.jpg";
 import cieszynThumb from "../assets/trips/thumbnails/cieszyn.jpg";
 import lichenThumb from "../assets/trips/thumbnails/lichen.jpg";
 import pragaThumb from "../assets/trips/thumbnails/praga.jpg";
+import pruszczLuminaThumb from "../assets/trips/thumbnails/pruszczLuminaPark.jpg";
+import jarmarkTorunBgdThumb from "../assets/trips/thumbnails/jarmarkTorun.jpg";
 
 export interface Trip {
   startDate: Date;
@@ -53,5 +57,23 @@ export const trips: Trip[] = [
     price: 650,
     imageUrl: pragaImg,
     thumbnailUrl: pragaThumb,
+  },
+  {
+    startDate: new Date("2025-11-22"),
+    endDate: new Date("2025-11-22"),
+    title: "LUMINA PARK Pruszcz Gdański",
+    description: "",
+    price: 155,
+    imageUrl: pruszczImg,
+    thumbnailUrl: pruszczLuminaThumb,
+  },
+  {
+    startDate: new Date("2025-12-06"),
+    endDate: new Date("2025-12-06"),
+    title: "Mikołajkowy Jarmark w Bydgoszczy i w Toruniu",
+    description: "",
+    price: 175,
+    imageUrl: jarmarkTorunBgdImg,
+    thumbnailUrl: jarmarkTorunBgdThumb,
   },
 ];
